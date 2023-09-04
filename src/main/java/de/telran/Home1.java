1a096ea47bed0cf76a4f899acdeb929bd0d91fd0
maratgmk <gmkorg109@gmail.com>
2023-09-04 23:51:27 +0300

    Home1.java

---
+++ src/main/java/de/telran/Home1.java
@@ -0,0 +1,21 @@
+package de.telran;
+
+public class Home1 {
+    public static void main(String[] args) {
+        char g = 'G';
+        byte a = 4;
+        int b = 89;
+        short c = 56;
+        float d = 4.7333436f;
+        double z = 4.355453532;
+        long h = 12121;
+        System.out.println("char: " +g );
+        System.out.println("byte: " + a);
+        System.out.println("int: " + b);
+        System.out.println("short: " + c);
+        System.out.println("float: " + d);
+        System.out.println("double: " + z);
+        System.out.println("long: " + h);
+
+    }
+}
